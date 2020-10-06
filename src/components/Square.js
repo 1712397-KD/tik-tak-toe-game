03cef9ff65d07835738f77846820942246482749
1712397-KD <1712397@student.hcmus.edu.vn>
2020-10-06 22:47:25 +0700

    no commit

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,8 +4,7 @@ function Square(props) {
   return (
     <button
       className="square"
-      onClick={props.onClick
-      }
+      onClick={props.onClick}
     >
       {props.value}
     </button>
